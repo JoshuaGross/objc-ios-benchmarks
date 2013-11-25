3392d9b0705aa43863ca04fd366491393c12341b
Joshua Gross <jgross@yahoo-inc.com>
2013-11-25 12:26:24 -0800

    Initial Commit

---
+++ BenchmarkInitWithCapacity/BenchmarkInitWithCapacity/JGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JGAppDelegate.h
+//  BenchmarkInitWithCapacity
+//
+//  Created by Joshua Gross on 11/25/13.
+//  Copyright (c) 2013 JGross. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BenchmarkInitWithCapacity/BenchmarkInitWithCapacity/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BenchmarkInitWithCapacity
+//
+//  Created by Joshua Gross on 11/25/13.
+//  Copyright (c) 2013 JGross. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JGAppDelegate class]));
+    }
+}
